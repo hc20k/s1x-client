8b3bcc7660f9ee95f6e014eae3b84a90b7ccc38b
h! <harrison.coutee@gmail.com>
2021-02-20 13:09:43 -0800

    More trickshot stuff
    Spawn hook
    Weapon damage hook

--- src/client/component/trickshot.cpp
+++ src/client/component/trickshot.cpp
@@ -6,6 +6,7 @@
 
 #include "trickshot.hpp"
 #include <utils/hook.hpp>
+#include <utils/string.hpp>
 
 game::dvar_t* ts_botLocationRadius;
 game::dvar_t* ts_enableNoSpread;
@@ -13,15 +14,18 @@ game::dvar_t* ts_enableNoSpread;
 game::vec3_t saved_location = { -1 };
 game::vec3_t saved_bot_location = { -1 };
 
-utils::hook::detour ls_playerdatagetintbyname_hook;
-utils::hook::detour bg_srand_hook;
+utils::hook::detour clientspawn_hook;
+utils::hook::detour bg_getdamage_hook;
+utils::hook::detour bg_getsurfacepenetrationdepth_hook;
 
-#define iprintln(ln) game::SV_GameSendServerCommand(0, game::svscmd_type::SV_CMD_CAN_IGNORE, ln)
+bool is_first_spawn = false;
+
+#define ssc(ln) game::SV_GameSendServerCommand(0, game::svscmd_type::SV_CMD_CAN_IGNORE, ln)
 
 namespace trickshot {
 
 	void save_location() {
-		iprintln("e \"^:Saved location.\"");
+		ssc("e \"^:Saved location.\"");
 
 		auto* ps = game::SV_GetPlayerstateForClientNum(0);
 		reinterpret_cast<void(*)(game::playerState_s*, game::vec3_t*)>(0x1402dc250)(ps, &saved_location); //  G_GetPlayerEyePosition
@@ -29,7 +33,7 @@ namespace trickshot {
 	}
 
 	void save_bot_location() {
-		iprintln("e \"^:Saved bot location.\"");
+		ssc("e \"^:Saved bot location.\"");
 
 		auto* ps = game::SV_GetPlayerstateForClientNum(0);
 		game::G_GetPlayerEyePosition(ps, &saved_bot_location);
@@ -51,7 +55,7 @@ namespace trickshot {
 
 	void n_key_pressed() {
 		if (saved_location[0] == -1) {
-			iprintln("e \"^1Error: saved location doesn't exist.\"");
+			ssc("e \"^1Error: saved location doesn't exist.\"");
 		}
 		else {
 			game::SetClientOrigin(&game::mp::g_entities[0], &saved_location, 0);
@@ -69,20 +73,20 @@ namespace trickshot {
 					return scheduler::cond_continue;
 				}
 
+				saved_location[0] = -1;
+				saved_bot_location[0] = -1;
+				is_first_spawn = false;
+
 				return scheduler::cond_end;
 			}, scheduler::pipeline::main, 1s);
-			command::execute("spawnBot 18", true);
+
+
+			//game::Dvar_FindVar("perk_bulletPenetrationMultiplier")->current.value = 30.f;
 		}
 
 		void give_random_class() {
 			auto ps = game::SV_GetPlayerstateForClientNum(0);
 			game::G_GivePlayerWeapon(ps, game::G_GetWeaponForName("defaultweapon_mp"), 0, 0, 0, 0, 0, 0);
-		}
-
-		int LiveStorage_PlayerDataGetIntByName(unsigned int p1, game::scr_string_t p2, int p3) {
-			int orig = ls_playerdatagetintbyname_hook.invoke<int>(p1, p2, p3);
-
-			return orig;
 		}
 
 		void BG_srand(int* out) {
@@ -91,6 +95,54 @@ namespace trickshot {
 			else
 				*out = (((*out * 0x343fd + 0x269ec3) * 0x343fd + 0x269ec3) * 0x343fd + 0x269ec3) * 0x343fd + 0x269ec3;
 		}
+
+		void on_host_spawned(game::mp::gentity_s* ent) {
+			ent->flags ^= 1; // god mode
+
+			if (!is_first_spawn) {
+				is_first_spawn = true;
+				command::execute(utils::string::va("spawnBot %d", game::Dvar_FindVar("party_maxplayers")->current.integer - 1), true);
+			}
+		}
+
+		void ClientSpawn(game::mp::gentity_s* ent, float* origin, float* angles) {
+			if (ent->client) {
+				int clientNum = ent->client->__pad0[0x5090];
+
+				if (clientNum == 0) {
+					on_host_spawned(ent);
+					if (saved_location[0] != -1) {
+						origin[0] = saved_location[0];
+						origin[1] = saved_location[1];
+						origin[2] = saved_location[2];
+					}
+				}
+				else {
+					if (saved_bot_location[0] != -1) {
+						origin[0] = saved_bot_location[0];
+						origin[1] = saved_bot_location[1];
+						origin[2] = saved_bot_location[2];
+					}
+				}
+			}
+			clientspawn_hook.invoke<void>(ent, origin, angles);
+		}
+
+		int BG_GetDamage(unsigned int weapon, unsigned int p2, unsigned long long p3) {
+			// BG_GetWeaponClass(weap, bool) 0x14016c8c0
+			auto weaponClass = reinterpret_cast<game::weapClass_t(*)(unsigned int, bool)>(0x14016c8c0)(weapon, true);
+		
+			if (weaponClass != game::weapClass_t::WEAPCLASS_SNIPER) {
+				return 0;
+			}
+
+			return 999;
+		}
+		
+		float BG_GetSurfacePenetrationDepth(unsigned int weapon, bool p2, int p3) {
+			return 999.f;
+		}
+
 	}
 
 	class component final : public component_interface
@@ -98,8 +150,10 @@ namespace trickshot {
 	public:
 		void post_unpack() override
 		{
-			ls_playerdatagetintbyname_hook.create(0x1403be860, &LiveStorage_PlayerDataGetIntByName); // Only snipers can kill
 			utils::hook::call(0x1402c9a1b, &BG_srand); // No spread
+			clientspawn_hook.create(0x1402db5a0, &ClientSpawn);
+			bg_getsurfacepenetrationdepth_hook.create(0x1401641a0, &BG_GetSurfacePenetrationDepth);
+			bg_getdamage_hook.create(0x140169f70, &BG_GetDamage);
 
 			command::add("ts_saveLocation", []()
 			{
@@ -114,19 +168,28 @@ namespace trickshot {
 			ts_botLocationRadius = game::Dvar_RegisterInt("ts_botLocationRadius", 50, 0, 500, game::DVAR_FLAG_SAVED, "The distance the bots can stray from the saved bot location.");
 			ts_enableNoSpread = game::Dvar_RegisterBool("ts_enableNoSpread", true, game::DVAR_FLAG_SAVED, "Disables weapon fire randomness.");
 
+			command::add("ts_addBot", []()
+				{
+					auto* ent = game::SV_AddTestClient(0);
+					game_console::print(0, "added bot @ %p", &ent);
+				});
+
 			command::add("trickshot", []()
 			{
 				command::execute("g_gametype dm", true);
 				command::execute("ui_gametype dm", true);
 				command::execute("scr_dm_scorelimit 1", true);
 				command::execute("scr_dm_timelimit 0", true);
+				command::execute("onlinemode 1", true);
 
 				// Patch bots so they don't kill
+				utils::hook::nop(0x14042a082, 5); // Bot_UpdateThreat
+				utils::hook::nop(0x14042a092, 5); // Bot_UpdateDistToEnemy
 				utils::hook::set(0x1404096A4, 0x00); // Bot_CanSeeEnemy
 
 				if (!game::CL_IsCgameInitialized())
 				{
-					game::SV_StartMap(0, "mp_terrace", false);
+					game::SV_StartMap(0, "mp_venus", false);
 				}
 				else command::execute("map_restart", true);
 
